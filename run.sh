453009c99e8a2f242cbab77a963e95f5a4e34475
tangjlh <135562423+tangjlh@users.noreply.github.com>
2024-05-30 12:01:57 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-echo "Please enter the GPU address"
-read GPU
-echo "Sellected GPU: $GPU"
-
-#train
-CUDA_VISIBLE_DEVICES=$GPU nohup python train.py --data_path '' --metric auc --dataset_type classification --save_dir '' --target_columns label --epochs 1 --ensemble_size 1 --num_folds 1 --batch_size 50 --aggregation mean --dropout 0.1 --save_preds >& out_name.out &
